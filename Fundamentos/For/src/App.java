f56ec4b2b9908666d65899ba0694c4c133fac383
Roberto-Dantas <robertodantas990@gmail.com>
2025-06-30 22:12:34 -0300

    main

--- Fundamentos/For/src/App.java
+++ Fundamentos/For/src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var scanner = new Scanner(System.in);
+        System.out.println("Bem-vindo ao programa de números!");
+        System.out.println("\nEscolha até que número você gostaria de ver: ");
+        int maxNumber = scanner.nextInt();
+        System.out.println("\nDeseja ver números pares ou ímpares? (Digite '1' para par ou 2 para 'ímpar')");
+        int choice = scanner.nextInt();
+
+        if(choice == 1){
+            for(int i = 0; i <= maxNumber; i++){
+                if(i %2 != 0){
+                    continue; 
+                }
+                System.out.println(i); // imprime apenas números pares
+            }
+        } else if(choice == 2){
+            for(int i = 0; i <= maxNumber; i++){
+                if(i %2 == 0){
+                    continue; 
+                }
+                System.out.println(i); // imprime apenas números ímpares
+        }
+        } else {
+            System.out.println("Opção inválida. Por favor, escolha 1 ou 2.");
+        }
+        
+        System.out.println("\nObrigado por usar nosso sistema!");
+        scanner.close();
     }
 }
